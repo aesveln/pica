989d05c0df01486a5782853c57fd618bd45b075a
angelicav620 <38836663+angelicav620@users.noreply.github.com>
2018-09-22 20:15:26 -0500

    Sales order funcionando ok con servicios rest

--- Java/toures-b2c.orders/src/main/java/co/com/toures/b2c/orders/controller/admcyo/SalesOrderController.java
+++ Java/toures-b2c.orders/src/main/java/co/com/toures/b2c/orders/controller/admcyo/SalesOrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import co.com.toures.b2c.orders.dto.admcyo.SalesOrderDTO;
 import co.com.toures.b2c.orders.model.admcyo.SalesOrderRequest;
@@ -28,6 +29,9 @@ public class SalesOrderController {
 	
 	ModelMapper modelMapper = new ModelMapper();
 	
+	static final String URL_HOTELES = "http://localhost:8087/HotelDann";
+	static final String URL_EVENTO = "http://localhost:8087/TuBoleta";
+	
 	@GetMapping("/salesorder/{status}")
 	public List<SalesOrderDTO> getSalesByStatus (@PathVariable(value = "status") String status)
 	{
@@ -41,14 +45,34 @@ public class SalesOrderController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value =  "/salesorder/cancelSaleOrder/{idsales}")
-	public int cancelSale  (@PathVariable(value = "idsales") int idsales)
+	public String cancelSale  (@PathVariable(value = "idsales") int idsales)
 	{
-		return salesService.cancelSales(idsales);
+		 RestTemplate restTemplate = new RestTemplate();
+		 int salecancel;
+		 String ret="";
+		 
+		 try
+		 {
+			 //POST METHOD
+			 String resultHoteles = restTemplate.postForObject(URL_HOTELES, idsales ,String.class);
+			 String resultEvento = restTemplate.postForObject(URL_EVENTO, idsales ,String.class);
+			 salecancel = salesService.cancelSales(idsales);
+			
+				 ret = "Orden cancelada";
+			
+		 }catch(Exception e)
+		 {
+			 ret="Se re puteo "+ e.getMessage();
+		 }
+		
+		 	 
+		 return ret;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value =  "/salesorder/salesCliente/{idcliente}")
 	public List<SalesOrderDTO> getSalesforClient (@PathVariable(value = "idcliente") int idcliente)
 	{
+		
 		return salesService.ordenesClientes(idcliente);
 	}
 }
