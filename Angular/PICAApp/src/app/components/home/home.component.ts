f24f30e073ec018fb146008e0425b11c09f5bd9a
javier91CB <37750494+javier91CB@users.noreply.github.com>
2018-09-24 20:54:50 -0500

    add connection to service product

--- Angular/PICAApp/src/app/components/home/home.component.ts
+++ Angular/PICAApp/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductoRequest } from '../../model/Request/ProductRequest';
+import { ProductService } from '../../services/product/product.service';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,10 @@ export class HomeComponent implements OnInit {
   items: Array<any> = [];
   viewCatalog: boolean;
   values = '';
-  constructor() {
+  product: ProductoRequest;
+  constructor(
+    private productService: ProductService
+  ) {
     this.items = [
       {name:  '../../../assets/images/img_mountains_wide.jpg'}
     ];
@@ -26,4 +30,27 @@ export class HomeComponent implements OnInit {
       this.viewCatalog = false;
     }
   }
+
+  getEventsFilter(){
+    debugger;
+        this.product = new ProductoRequest();
+    
+        this.product.arrivalDate = '';
+        this.product.departureDate = '';
+        this.product.pageNumber = 0;
+        this.product.pageSize = 10
+        this.product.return_date = '';
+        this.product.sortBy = '';
+        this.product.spectacleDate = '';
+    
+        this.productService.getAllProductos(this.product)
+          .subscribe(
+            (data)=> {
+              JSON.stringify(data)
+            },
+            error => {
+              console.log(error);
+            }
+          )
+      }
 }
---
+++ Angular/PICAApp/src/app/model/Dto/Products/DtoProduct.ts
@@ -0,0 +1,12 @@
+export class ProductoRequest {
+    cod: string;
+    cost: number;
+    description: string;
+    id: number;
+    imageRef: string;
+    lodgingType: string;
+    name: string;
+    productType: string;
+    spectacleType: string;
+    transportType: string;
+}
