91ad422a9f682b5009acc0aa5b95b5dc42442f30
narfco <carlos.corzo@outlook.es>
2018-10-06 20:25:40 -0500

    Creacion carpeta oms y eliminacion de archivos innecesarios.

--- Java/compile.sh
+++ Java/compile.sh
@@ -1,11 +1,7 @@
 cd toures-b2c.users
 mvn clean 
 mvn package
-#docker build -t toures-b2c.users .
-#docker run -p 8081:8081 --name toures-b2c.users -d toures-b2c.users
 cd ..
 cd toures-b2c.products
 mvn clean 
 mvn package
-#docker build -t toures-b2c.products .
-#docker run -p 8081:8081 --name toures-b2c.products -d toures-b2c.products
