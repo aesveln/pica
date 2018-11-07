70b6537d84851f22132726845fbb01d633ab8007
angelicav620 <38836663+angelicav620@users.noreply.github.com>
2018-11-07 17:31:41 -0500

    descuentos y validartc

--- Java/toures-b2c.users/src/main/java/co/com/toures/b2c/users/service/CustomerService.java
+++ Java/toures-b2c.users/src/main/java/co/com/toures/b2c/users/service/CustomerService.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.Response;
+import javax.xml.datatype.DatatypeConfigurationException;
+
 import java.util.Optional;
 
 @Service
@@ -19,25 +21,50 @@ public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
     
-    @Autowired
-    CustomerClient customerclient = new CustomerClient();
+//    @Autowired
+//    CustomerClient customerclient = new CustomerClient();
+//    
+//    @Autowired
+//    BpelClient bpel = new BpelClient();
 
     ModelMapper modelMapper = new ModelMapper();
 
 
-    public String loginUser(CustomerLoginRequest customerLoginRequest) {
+    public String loginUser(CustomerLoginRequest customerLoginRequest) throws DatatypeConfigurationException {
 
         Customer cust = customerRepository.findByEmail(customerLoginRequest.getCorreo());
         CustomerDTO customerDTO = modelMapper.map(cust, CustomerDTO.class);
         boolean status = false;
-        double descuento;
+        boolean tarjetaValida = false;
+        double descuento = 0;
+        String valida = "";
         if (customerDTO.getPass().toLowerCase().equals(customerLoginRequest.getPassword().toLowerCase())) {
             status = true;
-            descuento = getDescuento(customerDTO.getCategory());
+            if(customerDTO.getCategory().equals("Dorado"))
+            {
+            	descuento = 0.25;
+            }
+            if(customerDTO.getCategory().equals("Plateado"))
+            {
+            	descuento = 0.15;
+            }
+            if(customerDTO.getCategory().equals("Platino"))
+            {
+            	descuento = 0.35;
+            }
+            
+          
+            tarjetaValida = validatc(customerDTO.getCreditcard_number());
+           
+            if(tarjetaValida) {
+            	valida="Tarjeta Valida";
+            }else {
+            	valida="Tarjeta NO Valida";
+            }
             
         }
 
-        return co.com.toures.b2c.users.conf.JwtApplication.JwtToken(status, "asd", customerDTO);
+        return co.com.toures.b2c.users.conf.JwtApplication.JwtToken(status, "asd", customerDTO, descuento, valida);
     }
 
     public CustomerResponse createCustomer(CustomerRequest customerRequest) {
@@ -74,8 +101,25 @@ public class CustomerService {
         return cr;
     }
     
-    public double getDescuento (String categoria)
+    public double getDescuento (String categoria) throws DatatypeConfigurationException
 	{
-		return customerclient.getRespuestaServicio(categoria);
+		//return customerclient.getRespuestaServicio(categoria);
+    	return 0;
 	}
+    
+    public boolean validatc (String tc)
+    {
+    	boolean valida = false;
+    	
+    	if(tc.length()>=12 && tc.length()<=16)
+    	{
+    		if(tc.startsWith("4") || tc.startsWith("5") || tc.startsWith("6"))
+    		{
+    			valida=true;
+    		}else {
+    			valida=false;
+    		}
+    	}
+    	return valida;
+    }
 }
